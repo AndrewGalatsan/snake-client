0609334caceedd1df294184de05e0b326e3b87ce
AndrewGalatsan <agalatsan@hotmail.com>
2022-12-11 23:55:41 +0000

    Refactored play.js.

--- play.js
+++ play.js
@@ -1,6 +1,5 @@
-// const net = require("net");
 const { connect } = require('./client');
-
-
-console.log("Connecting ...");
-connect();
+const { setupInput } = require('./input');
+console.log('Connecting ...');
+const conn = connect();
+setupInput(conn);
