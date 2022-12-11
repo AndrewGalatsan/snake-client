5fb3560943f3db6c96f045fa6e9498ed8564c546
AndrewGalatsan <agalatsan@hotmail.com>
2022-12-11 23:43:03 +0000

    Making updates by adding a name to the snake.

--- client.js
+++ client.js
@@ -9,10 +9,17 @@ const connect = function () {
   conn.setEncoding("utf8");
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
+    conn.write('Name: BOB');
   });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
   return conn;
 };
 
-module.exports = {
-  connect
-}
+
+
+
+module.exports = { connect };
